f4da9ec5cb599150607700acebc65a8b79c65c8b
Josh Sikes <josh.sikes@ramseysolutions.com>
2023-06-21 12:59:32 -0500

    additional content

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -2,6 +2,9 @@ function AboutMe (){
   return (
     <div>
       <h2>About Me:</h2>
+      <p>
+        This is the section where I talk about myself... what would you like to know? Am I interesting? Maybe, maybe not. But you're here, so I guess I'll tell you anyway.
+      </p>
     </div>
   );
 };
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,6 +2,32 @@ function Skills (){
   return (
     <div>
       <h2>My Skills, Languages, & Frameworks:</h2>
+      <ul>
+        <li>
+          HTML
+        </li>
+        <li>
+          CSS
+        </li>
+        <li>
+          JavaScript
+        </li>
+        <li>
+          Ruby
+        </li>
+        <li>
+          TypeScript
+        </li>
+        <li>
+          Rails
+        </li>
+        <li>
+          React
+        </li>
+        <li>
+          SQL
+        </li>
+      </ul>
     </div>
   );
 };

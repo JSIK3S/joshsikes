6df6c14c063deba8aea48e3d10b34b49f491186f
Josh Sikes <josh.sikes@ramseysolutions.com>
2023-06-24 09:17:05 -0500

    adds Nav section

--- src/components/PageSections/Header.tsx
+++ src/components/PageSections/Header.tsx
@@ -1,9 +1,9 @@
+import { Nav } from "./Nav";
+
 function Header (){
   return (
     <header>
-      <div>
-        <h2>Header Section</h2>
-      </div>
+      <Nav />
     </header>
   );
 };
---
+++ src/components/PageSections/Nav.tsx
@@ -0,0 +1,15 @@
+function Nav (){
+  return (
+    <div>
+      <h4>Nav Links</h4>
+      <ul>
+        <li>Main</li>
+        <li>About Me</li>
+        <li>Skills</li>
+        <li>Recommends</li>
+      </ul>
+    </div>
+  );
+};
+
+export {Nav};

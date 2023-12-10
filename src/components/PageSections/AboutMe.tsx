2904a10e71637bb7750367a9971d61d1c2a8ae4b
Josh Sikes <josh.sikes@ramseysolutions.com>
2023-12-09 19:32:10 -0600

    adds about me content

--- src/components/PageSections/AboutMe.tsx
+++ src/components/PageSections/AboutMe.tsx
@@ -3,10 +3,27 @@ function AboutMe() {
     <div className="h-screen flex items-center bg-slate-300">
       <section id="about" className="m-8">
         <h2 className="mb-16 text-3xl">About Me:</h2>
-        <p>
+        <p className="mb-8">
           This is the section where I talk about myself... what would you like
           to know? Am I interesting? Maybe, maybe not. But you're here, so I
           guess I'll tell you anyway.
+        </p>
+        <p className="mb-8">
+          After spending the first 11 years of my career working in Recruiting &
+          Human Resources primarily focusing on recruiting technical talent, I
+          finally decided to take the plunge myself to become a Software
+          Engineer! Thankfully, the company I worked for had a great platform to
+          help its team members transition to new roles. Initially, I was mostly
+          self taught, going through a few different online coding programs.
+          Once I formally moved into Product Engineering at work, I was also
+          able to go through an extensive mentorship program designed to level
+          up its junior engineers.
+        </p>
+        <p className="mb-8">
+          Since then I haven't looked back, and I've worked on websites,
+          internal services, batch jobs, you name it! I'm super grateful for the
+          opportunity I had to become and engineer, and I've loved every second
+          of it! Here's to learning more!
         </p>
       </section>
     </div>

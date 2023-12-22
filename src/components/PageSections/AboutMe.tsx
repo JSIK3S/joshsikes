bd28a2c7944f1b302b8a7d19de80a92a832d3717
Josh Sikes <josh.sikes@ramseysolutions.com>
2023-12-21 21:59:00 -0600

    more styling

--- src/components/PageSections/AboutMe.tsx
+++ src/components/PageSections/AboutMe.tsx
@@ -1,7 +1,7 @@
 function AboutMe() {
   return (
-    <div className="h-screen flex flex-col-reverse items-center justify-between bg-slate-300 sm:flex-row">
-      <section id="about" className="m-3">
+    <div className="h-screen flex flex-col-reverse items-center justify-around bg-slate-300 sm:flex-row">
+      <section id="about" className="m-3 md:max-w-lg">
         <h2 className="mb-16 text-3xl">About Me:</h2>
         <p className="mb-8 text-lg max-w-prose">
           After spending the first 11 years of my career working in Recruiting &
@@ -21,7 +21,7 @@ function AboutMe() {
           of it! Here's to learning more!
         </p>
       </section>
-      <section className="m-3">
+      <section className="m-3 md:max-w-lg">
         <img
           src="/joshsikes/assets/profilepic.jpg"
           alt="Josh Sikes Headshot Photo"

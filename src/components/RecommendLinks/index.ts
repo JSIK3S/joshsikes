c83f7b8d34ecec99a8296ee12c0986f93ebed96a
Josh Sikes <josh.sikes@ramseysolutions.com>
2026-03-12 16:43:44 -0500

    Removes unused files

--- src/components/RecommendLinks/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./BioJazz";
-export * from "./Leon";
-export * from "./MacArthur";
-export * from "./Pragmatic";
-export * from "./Quadratic";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export * from "./navBarMenus";
 export * from "./pageSections";
-export * from "./recommendLinks";
 export * from "./socialLinks";
 export * from "./techIcons";
 export * from "./ScrollToTopIcon";
